6c6f1f42c99a6e514086decbe2a5c929a0a81342
majortom327 <me@valentin-thomas.com>
2023-05-22 12:31:17 +0200

    Add converter

--- app/routes/_app.converter/index.tsx
+++ app/routes/_app.converter/index.tsx
@@ -1,18 +1,30 @@
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { useFetcher } from "@remix-run/react";
 import { json } from "@vercel/remix";
+import { useDebounce, useDebounceEffect } from "ahooks";
 import { pathOr, values } from "ramda";
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { Button, Card, Input, InputGroup, Select } from "react-daisyui";
 import { HiOutlineSwitchHorizontal } from "react-icons/hi";
 import { match } from "ts-pattern";
+import zod from "zod";
 import CurrencyEnum from "~/refs/CurrencyEnum";
+
+import FinanceApi from "~/lib/finance";
 
 import { useOptionalUser } from "~/hooks/useUser";
 
 type LoaderData = {};
+type ActionData = {
+  rate: number;
+  from: CurrencyEnum;
+  to: CurrencyEnum;
+  value: number;
+};
 
-export const loader: LoaderFunction = async () => {
+const finance = new FinanceApi();
+
+export const loader: LoaderFunction = async ({ request }) => {
   return json<LoaderData>({});
 };
 
@@ -27,7 +39,8 @@ type ReducerAction =
   | { type: "swap" }
   | { type: "selectFrom"; currency: CurrencyEnum }
   | { type: "selectTo"; currency: CurrencyEnum }
-  | { type: "changeFromValue"; value: number };
+  | { type: "changeFromValue"; value: number }
+  | { type: "changeToValue"; value: number };
 
 export const AppConverter = () => {
   const user = useOptionalUser();
@@ -68,6 +81,12 @@ export const AppConverter = () => {
             fromValue: value,
           };
         })
+        .with({ type: "changeToValue" }, ({ value }) => {
+          return {
+            ...state,
+            toValue: value,
+          };
+        })
         .otherwise(() => state);
     },
     {
@@ -79,6 +98,43 @@ export const AppConverter = () => {
       toCurrency: defaultCurrency as CurrencyEnum,
     }
   );
+
+  useDebounceEffect(
+    () => {
+      if (currentState.fromValue === 0) {
+        return;
+      }
+
+      fetcher.submit(
+        {
+          from: currentState.fromCurrency,
+          to: currentState.toCurrency,
+          value: currentState.fromValue.toString(),
+        },
+        { method: "POST" }
+      );
+    },
+    [
+      currentState.fromCurrency,
+      currentState.fromValue,
+      currentState.toCurrency,
+    ],
+    { wait: 500 }
+  );
+
+  useEffect(() => {
+    if (fetcher.data?.value === undefined) {
+      return;
+    }
+
+    // Cleanup the value
+    const value = Math.round(pathOr(0, ["data", "value"], fetcher) * 100) / 100;
+
+    dispatchState({
+      type: "changeToValue",
+      value,
+    });
+  }, [fetcher.data?.value]);
 
   return (
     <>
@@ -130,11 +186,22 @@ export const AppConverter = () => {
                 className="w-full"
                 type="number"
                 value={currentState.toValue}
+                readOnly
                 disabled
               />
-              <Select value={currentState.toCurrency}>
+              <Select
+                value={currentState.toCurrency}
+                onChange={(e) =>
+                  dispatchState({
+                    type: "selectTo",
+                    currency: e.target.value as CurrencyEnum,
+                  })
+                }
+              >
                 {Object.values(CurrencyEnum).map((currency) => (
-                  <option value={currency}>{currency}</option>
+                  <option key={currency} value={currency}>
+                    {currency}
+                  </option>
                 ))}
               </Select>
             </InputGroup>
@@ -145,8 +212,27 @@ export const AppConverter = () => {
   );
 };
 
-export const action: ActionFunction = async () => {
-  return json({});
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+
+  const params = zod
+    .object({
+      from: zod.nativeEnum(CurrencyEnum),
+      to: zod.nativeEnum(CurrencyEnum),
+      value: zod.coerce.number(),
+    })
+    .parse(Object.fromEntries(formData.entries()));
+
+  const rate = await finance.getExchangeRate(params.from, params.to);
+
+  console.log("Rate", { rate, params });
+
+  return json<ActionData>({
+    rate,
+    from: params.from,
+    to: params.to,
+    value: params.value * rate,
+  });
 };
 
 export default AppConverter;
